af3c0394fe6e433a112554e8b57acff1f8d000af
eaysin <eaysin.dev@gmail.com>
2025-07-06 07:09:04 +0600

    Removed `.pnp.cjs` file from the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const handleClick = () => {
+        const params = new URLSearchParams({
+            response_type: "code",
+            client_id: "86htpoghv1ptpr",
+            redirect_uri: "http://localhost:5000/api/v1/linkedin/auth/callback",
+            scope: "openid profile email",
+        });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        window.location.href = `https://www.linkedin.com/oauth/v2/authorization?${params.toString()}`;
+    };
+
+    return (
+        <div className="page-layout">
+            <Button onClick={handleClick}>
+                Login with LinkedIn
+            </Button>
+        </div>
+    );
 }
 
-export default App
+export default App;
